8a41bd76c787c139f506ca48269fdf4bc8d6e5c7
open-dxp-stack <stack@opendxp.ch>
2026-02-04 10:59:49 +0100

    Update PaymentProvider.php

--- .github/ci/files/var/classes/DataObject/OnlineShopOrder/PaymentProvider.php
+++ .github/ci/files/var/classes/DataObject/OnlineShopOrder/PaymentProvider.php
@@ -8,32 +8,6 @@ class PaymentProvider extends \OpenDxp\Model\DataObject\Objectbrick {
 
 protected $brickGetters = ['PaymentProviderUnzer'];
 
-
-protected \OpenDxp\Model\DataObject\Objectbrick\Data\PaymentProviderPayPalSmartButton|null $PaymentProviderPayPalSmartButton = null;
-
-/**
-* @return \OpenDxp\Model\DataObject\Objectbrick\Data\PaymentProviderPayPalSmartButton|null
-*/
-public function getPaymentProviderPayPalSmartButton(bool $includeDeletedBricks = false)
-{
-	if(!$includeDeletedBricks &&
-		isset($this->PaymentProviderPayPalSmartButton) &&
-		$this->PaymentProviderPayPalSmartButton->getDoDelete()) {
-			return null;
-	}
-	return $this->PaymentProviderPayPalSmartButton;
-}
-
-/**
-* @param \OpenDxp\Model\DataObject\Objectbrick\Data\PaymentProviderPayPalSmartButton|null $PaymentProviderPayPalSmartButton
-* @return $this
-*/
-public function setPaymentProviderPayPalSmartButton(?\OpenDxp\Model\DataObject\Objectbrick\Data\PaymentProviderPayPalSmartButton $PaymentProviderPayPalSmartButton): static
-{
-	$this->PaymentProviderPayPalSmartButton = $PaymentProviderPayPalSmartButton;
-	return $this;
-}
-
 protected \OpenDxp\Model\DataObject\Objectbrick\Data\PaymentProviderUnzer|null $PaymentProviderUnzer = null;
 
 /**
